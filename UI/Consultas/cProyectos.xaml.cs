da9e58d55c150643b1ee22cbc2c021e80b056526
DT <DT@DESKTOP-NNDF78P>
2021-11-08 20:18:27 -0400

    Agregando UI

--- UI/Consultas/cProyectos.xaml.cs
+++ UI/Consultas/cProyectos.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using P2_AP1_Ismarlin2018_0846.BLL;
+using P2_AP1_Ismarlin2018_0846.DAL;
+using P2_AP1_Ismarlin2018_0846.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +29,35 @@ namespace P2_AP1_Ismarlin2018_0846.UI.Consultas
 
         private void BuscarBoton_Click(object sender, RoutedEventArgs e)
         {
+            var lista = new List<Proyectos>();
 
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        lista = ProyectosBLL.GetList(p => p.Tiempo == Convert.ToInt32(CriterioTextBox.Text));
+                        break;
+
+                    case 1:
+                        lista = ProyectosBLL.GetList(p => p.ProyectoId == Convert.ToInt32(CriterioTextBox.Text));
+                        break;
+                    case 2:
+                        lista = ProyectosBLL.GetList(p => p.Descripcion == CriterioTextBox.Text);
+                        break;
+                }
+            }
+            else
+            {
+                lista = ProyectosBLL.GetList(c => true);
+            }
+            if(lista == null)
+            {
+                MessageBox.Show("Proyecto no encontrado", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = lista;
         }
+
     }
 }
--- UI/Registros/rProyectos.xaml.cs
+++ UI/Registros/rProyectos.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using P2_AP1_Ismarlin2018_0846.BLL;
+using P2_AP1_Ismarlin2018_0846.DAL;
+using P2_AP1_Ismarlin2018_0846.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,44 +22,140 @@ namespace P2_AP1_Ismarlin2018_0846.UI.Registros
     /// </summary>
     public partial class rProyectos : Window
     {
+        Proyectos proyecto = new Proyectos();
         public rProyectos()
         {
             InitializeComponent();
+            TipoTareaComboBox.ItemsSource = TipoTareasBLL.GetList();
+            TipoTareaComboBox.SelectedValuePath = "TipoId";
+            TipoTareaComboBox.DisplayMemberPath = "Descripcion";
+            Limpiar();
         }
-
+        private void Limpiar()
+        {
+            this.proyecto = new Proyectos();
+            this.proyecto.Fecha = DateTime.Now;
+            this.DataContext = proyecto;
+            RequerimientoTextBox.Clear();
+            TiempoTextBox.Clear();
+        }
         private void BuscarBoton_Click(object sender, RoutedEventArgs e)
         {
+            var encontrado = ProyectosBLL.Buscar(Convert.ToInt32(ProyectoIdTextBox.Text));
 
+            if (encontrado != null)
+            {
+                this.proyecto = encontrado;
+                this.DataContext = null;
+                this.DataContext = proyecto;
+            }
+            else
+            {
+                Limpiar();
+                MessageBox.Show("Proyecto no encontrado", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void TiempoTextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            try
+            {
+                if (TiempoTextBox.Text.Any(char.IsLetter))
+                {
+                    TiempoTextBox.BorderBrush = new SolidColorBrush(Colors.Red);
+                    MessageBox.Show("En el tiempo solo debe ingresar numeros", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    TiempoTextBox.BorderBrush = new SolidColorBrush(Colors.Black);
+                }
+            }
+            catch
+            {
+                TiempoTextBox.Foreground = SystemColors.ControlTextBrush;
+            }
         }
 
         private void AgregarBoton_Click(object sender, RoutedEventArgs e)
         {
-
+            Contexto context = new Contexto();
+            proyecto.Tiempo += Convert.ToInt32(TiempoTextBox.Text);
+            proyecto.Detalle.Add(new ProyectosDetalle(Convert.ToInt32(TipoTareaComboBox.SelectedValue.ToString()), proyecto.ProyectoId, RequerimientoTextBox.Text, Convert.ToInt32(TiempoTextBox.Text)));
+            this.DataContext = null;
+            this.DataContext = proyecto;
+            RequerimientoTextBox.Clear();
+            TiempoTextBox.Clear();
+            TipoTareaComboBox.Text = " ";
         }
 
         private void GuardarBoton_Click(object sender, RoutedEventArgs e)
         {
+            bool paso = false;
 
+            if (Validar())
+                paso = ProyectosBLL.Guardar(proyecto);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Guardado!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Error al guardar", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
         private void NuevoBoton_Click(object sender, RoutedEventArgs e)
         {
-
+            Limpiar();
         }
 
         private void RemoverBoton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (TareasDataGrid.Items.Count >= 1 && TareasDataGrid.SelectedIndex <= TareasDataGrid.Items.Count - 1)
+            {
+                ProyectosDetalle project = (ProyectosDetalle)TareasDataGrid.SelectedValue;
+                proyecto.Tiempo -= project.Tiempo;
+                proyecto.Detalle.RemoveAt(TareasDataGrid.SelectedIndex);
+                this.DataContext = null;
+                this.DataContext = proyecto;
+            }
         }
 
         private void EliminarBoton_Click(object sender, RoutedEventArgs e)
         {
+            Proyectos existe = ProyectosBLL.Buscar(this.proyecto.ProyectoId);
 
+            if (existe == null)
+            {
+                MessageBox.Show("Proyecto no encontrado", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            else
+            {
+                ProyectosBLL.Eliminar(this.proyecto.ProyectoId);
+                MessageBox.Show("Eliminado", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+                Limpiar();
+            }
+        }
+        public bool Validar()
+        {
+            bool esValido = true;
+
+            if (FechaDatePicker.SelectedDate <= DateTime.Now)
+            {
+                MessageBox.Show("Debe elegir una fecha mayor a la actual", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                esValido = false;
+            }
+            if (DescripcionTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("Debe ingresar una descripcion", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                esValido = false;
+            }
+            return esValido;
         }
     }
 }
