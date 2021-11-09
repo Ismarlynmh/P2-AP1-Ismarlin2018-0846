fd6d47276caaf974ac8dabecccdbbe2d23047f68
DT <DT@DESKTOP-NNDF78P>
2021-11-08 20:16:35 -0400

    Agregando la BLL

---
+++ BLL/ProyectosBLL.cs
@@ -0,0 +1,176 @@
+﻿using P2_AP1_Ismarlin2018_0846.DAL;
+using P2_AP1_Ismarlin2018_0846.Entidades;
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace P2_AP1_Ismarlin2018_0846.BLL
+{
+    public class ProyectosBLL
+    {
+        public static bool Guardar(Proyectos proyecto)
+        {
+            if (!Existe(proyecto.ProyectoId))
+                return Insertar(proyecto);
+            else
+                return Modificar(proyecto);
+        }
+
+        private static bool Insertar(Proyectos proyecto)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                contexto.Proyectos.Add(proyecto);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        private static bool Modificar(Proyectos proyecto)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Database.ExecuteSqlRaw($"Delete FROM ProyectosDetalle Where ProyectoId={proyecto.ProyectoId}");
+
+                foreach (var item in proyecto.Detalle)
+                {
+                    contexto.Entry(item).State = EntityState.Added;
+                }
+
+                contexto.Entry(proyecto).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var proyecto = ProyectosBLL.Buscar(id);
+
+                if (proyecto != null)
+                {
+                    contexto.Proyectos.Remove(proyecto);
+                    paso = contexto.SaveChanges() > 0;
+                }
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static Proyectos Buscar(int id)
+        {
+            Proyectos proyecto = new Proyectos();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                proyecto = contexto.Proyectos.Include(p => p.Detalle)
+                    .Where(p => p.ProyectoId == id)
+                    .SingleOrDefault();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return proyecto;
+        }
+
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Proyectos.Any(p => p.ProyectoId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+
+        public static List<Proyectos> GetList()
+        {
+            List<Proyectos> lista = new List<Proyectos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Proyectos.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+        public static List<Proyectos> GetList(Expression<Func<Proyectos, bool>> criterio)
+        {
+            List<Proyectos> lista = new List<Proyectos>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Proyectos.Where(criterio).AsNoTracking().ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
---
+++ BLL/TipoTareasBLL.cs
@@ -0,0 +1,33 @@
+﻿using P2_AP1_Ismarlin2018_0846.DAL;
+using P2_AP1_Ismarlin2018_0846.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Ismarlin2018_0846.BLL
+{
+    public class TipoTareasBLL
+    {
+        public static List<TipoTareas> GetList()
+        {
+            List<TipoTareas> lista = new List<TipoTareas>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.TipoTareas.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
